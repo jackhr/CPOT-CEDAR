8e652dbe43d5a8aa9c3ef48f104d5be6c29f1d16
jackhr <jc2o@mac.com>
2025-08-21 11:09:20 -0400

    Stub up basic repo with under-construction page

---
+++ includes/head.php
@@ -0,0 +1,46 @@
+<?php require_once 'connection.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $company_name; ?></title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <!-- favicon begin -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/images/favicon/site.webmanifest">
+    <!-- favicon end -->
+
+    <!-- BEGIN FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- END FONTS -->
+
+    <!-- BEGIN PLUGINS -->
+
+    <!-- BEGIN PLUGINS CSS -->
+    <link rel="stylesheet" href="/assets/js/Flickity/css/flickity.min.css" media="screen">
+    <link rel="stylesheet" href="/assets/js/Flickity/css/flickity-fade.css" media="screen">
+    <link rel="stylesheet" href="/assets/js/sweetalert2/styles/sweetalert2.min.css" media="screen">
+    <!-- END PLUGINS CSS -->
+
+    <!-- BEGIN PLUGINS JS -->
+    <script src="/assets/js/jquery/jquery-3.7.1.min.js"></script>
+    <script src="/assets/js/hongymagic-jQuery.serializeObject/dist/jquery.serializeObject.min.js"></script>
+    <script src="/assets/js/Flickity/js/flickity.pkgd.min.js"></script>
+    <script src="/assets/js/Flickity/js/flickity-fade.js"></script>
+    <script src="/assets/js/Flickity/js/flickity-imagesloaded.js"></script>
+    <script src="/assets/js/sweetalert2/js/sweetalert2.all.min.js"></script>
+    <script src="/assets/js/waypoints/lib/jquery.waypoints.min.js"></script>
+    <!-- END PLUGINS JS -->
+
+    <!-- END PLUGINS -->
+
+    <script src="/assets/js/main.js" defer></script>
+</head>
