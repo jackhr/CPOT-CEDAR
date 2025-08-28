4e1e7959507b6fd93f4c1426e0e895478b4aa04d
jackhr <jc2o@mac.com>
2025-08-27 21:17:36 -0400

    Scrub up basic contact intro of a page

---
+++ contact/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Contact Us</h1>
+            <p>Have questions or want to book an experience? Reach out to our friendly team—we’re here to help you plan your creative visit!</p>
+            <button class="enquiry-btn">Reserve Your Event</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
