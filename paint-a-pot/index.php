45565ef5b96d9ea040b3e564f8f6f41628aae6db
jackhr <jc2o@mac.com>
2025-08-27 21:10:41 -0400

    Scrub up basic paint a pot intro of a page

---
+++ paint-a-pot/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Paint A Pot</h1>
+            <p>Come to our studio, pick a pre-fired pot, and let your creativity flow! Pay once, paint your masterpiece, and enjoy a fun, relaxing experience for everyone.</p>
+            <button class="enquiry-btn">Reserve Your Spot</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
