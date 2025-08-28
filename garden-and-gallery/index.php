072842f8b83c40d1d2a8d60196d1f1d8483a8870
jackhr <jc2o@mac.com>
2025-08-27 21:17:51 -0400

    Scrub up basic garden and gallery intro of a page

---
+++ garden-and-gallery/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Garden & Gallery</h1>
+            <p>Stroll through our serene garden and explore the gallery filled with unique, handcrafted pieces. A perfect blend of nature and art for inspiration and relaxation.</p>
+            <button class="enquiry-btn">Reserve Your Event</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
