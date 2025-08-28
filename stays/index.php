0a7b69d7aed61a218b0d67e836e8f376e1b507c9
jackhr <jc2o@mac.com>
2025-08-27 21:17:59 -0400

    Scrub up basic stays intro of a page

---
+++ stays/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Stays</h1>
+            <p>Book a cozy Airbnb stay right at our art studio! Enjoy a peaceful getaway where you can unwind, explore your creativity, and make art just steps from your door.</p>
+            <button class="enquiry-btn">Reserve Your Event</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
