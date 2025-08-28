9263ed4f509347ea2ce450d328de35688963379b
jackhr <jc2o@mac.com>
2025-08-27 21:17:29 -0400

    Scrub up basic about intro of a page

---
+++ about/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>About Us</h1>
+            <p>Learn the story behind our studio—a welcoming place where art, community, and fun come together for all ages to explore their creativity.</p>
+            <button class="enquiry-btn">Reserve Your Event</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
