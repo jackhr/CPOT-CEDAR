87f0814c92c45fe00c1d5f37cabcee70df081377
jackhr <jc2o@mac.com>
2025-08-27 21:17:42 -0400

    Scrub up basic events intro of a page

---
+++ events/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body id="sconces">
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Events</h1>
+            <p>Host birthdays, team outings, or special gatherings in our art studio! Enjoy a unique, creative experience where everyone can paint, laugh, and make memories together.</p>
+            <button class="enquiry-btn">Reserve Your Event</button>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
