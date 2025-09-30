ccb1d625a26e6afe39075611ad8944f5f5dc10e8
jackhr <jc2o@mac.com>
2025-09-29 21:52:16 -0400

    Add nav to index page

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php require_once __DIR__ . '/includes/head.php'; ?>
 
-<body></body>
+<body>
+
+    <?php require_once __DIR__ . '/includes/nav.php'; ?>
+
+</body>
 
 <?php require_once __DIR__ . '/includes/footer.php'; ?>
