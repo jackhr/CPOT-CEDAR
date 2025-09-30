cedce8c9383bafc1405a89aef11da5914a727da6
jackhr <jc2o@mac.com>
2025-09-29 21:51:42 -0400

    Add images

--- paint-a-pot/index.php
+++ paint-a-pot/index.php
@@ -11,6 +11,65 @@
         </div>
     </section>
 
+    <section id="steps">
+        <div class="inner">
+            <div class="step">
+                <div>
+                    <h2>Step 1</h2>
+                    <p>Book your date (non-refundable) and pick the pot you wish to decorate. Or wait until you are in the gallery to see various options available.</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-1.png" alt="">
+                </div>
+            </div>
+            <div class="step">
+                <div>
+                    <h2>Step 2</h2>
+                    <p>Upon arrival, you will be encouraged to chose a table area to offload the belongings, nibbles etc. you may have brought with you.</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-2.jpg" alt="">
+                </div>
+            </div>
+            <div class="step">
+                <div>
+                    <h2>Step 3</h2>
+                    <p>Then you will be shown our selection of blank pots from which to chose as many as you feel you can complete within the two hours.</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-3.jpg" alt="">
+                </div>
+            </div>
+            <div class="step">
+                <div>
+                    <h2>Step 4</h2>
+                    <p>Colors and tools to decorate your pot are to be provided. If you get at all anxious about “creative block” weve put together some useful “inspirational material” booklet to get you going.</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-4.jpg" alt="">
+                </div>
+            </div>
+            <div class="step">
+                <div>
+                    <h2>Step 5</h2>
+                    <p>Once decorated the pots remain at the studio for up to 4 days to be glossed fired and finished.</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-5.png" alt="">
+                </div>
+            </div>
+            <div class="step">
+                <div>
+                    <h2>Step 6</h2>
+                    <p>The studio will contact you when your finished pieces are ready to collect and take home to dazzle ypour family and friends!</p>
+                </div>
+                <div>
+                    <img src="/assets/images/paint-a-pot/steps/step-6.jpeg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
 
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
